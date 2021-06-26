ac704e830c9da4aed7537f5ece36a9a092352f65
Zikun Wang <z1wang@ucsd.edu>
2021-06-27 03:05:36 +0800

    Updates

---
+++ precache-manifest.1fbea37e287116146318fe0adb96f2eb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba7f2a949e7b50e049c80264ebcd612",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf18d5762ae3cff6b4df",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "70566c81bffdb10e13fd",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "0d4ec23ac5dd2fa7ff91",
+    "url": "/static/css/main.02a41b25.chunk.css"
+  },
+  {
+    "revision": "8d949d59ebe3477d70a6",
+    "url": "/static/js/2.ef512996.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.ef512996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf18d5762ae3cff6b4df",
+    "url": "/static/js/3.4107b202.chunk.js"
+  },
+  {
+    "revision": "70566c81bffdb10e13fd",
+    "url": "/static/js/4.545c8b17.chunk.js"
+  },
+  {
+    "revision": "0d4ec23ac5dd2fa7ff91",
+    "url": "/static/js/main.8f82c0c3.chunk.js"
+  },
+  {
+    "revision": "0de860d14cb90a03adf4",
+    "url": "/static/js/runtime-main.447c0dda.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "76fc614d0c90357bb1e5b4946565d059",
+    "url": "/static/media/corona.76fc614d.jpeg"
+  },
+  {
+    "revision": "3952f41bd410b0f9b33c3242dc17ccda",
+    "url": "/static/media/cour.3952f41b.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e49be36d46c932ef08cc9b5c46be733f",
+    "url": "/static/media/grosafe.e49be36d.JPG"
+  },
+  {
+    "revision": "9a56a2918003b974ac63fc6012525cfb",
+    "url": "/static/media/gzlz.9a56a291.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "d7d8ac16fbd3f8fa2e13d290c9afdf45",
+    "url": "/static/media/ossc.d7d8ac16.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
